6537d44acd4a323cbf67209f88a2e4e505b01640
Soumya <soumyakapu@gmail.com>
2023-12-25 22:27:43 +0530

    intial commits

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <NavBar/>
     {/* <BrowserRouter> */}
     <Routes>
+      
       {/* <Route path="/core" element={<CoreJava/>}/> */}
       </Routes>
     {/* </BrowserRouter> */}
